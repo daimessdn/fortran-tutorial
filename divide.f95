bc0f765851f277c929121f6f2c3a954d36022216
daimessdn@gmail.com <daimessdn@gmail.com>
2023-08-04 12:32:36 +0700

    added operator assignment

---
+++ divide.f95
@@ -0,0 +1,14 @@
+program divide
+
+    implicit none
+
+    real :: x, y
+
+    print *, "enter a number:"
+    read *, x
+
+    y = x / 3
+
+    print *, y
+
+end program divide
---
+++ sum.f95
@@ -0,0 +1,15 @@
+program sum
+
+    implicit none
+
+    real :: x, y, ans
+
+    print *, "enter 2 numbers"
+    read *,x
+    read *,y
+
+    ans = x + y
+
+    print *, "sum of 2 numbers is ", ans
+
+end program sum
